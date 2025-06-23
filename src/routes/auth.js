89f83de3b158891906e0c7bbecdf62306a83ff2a
Cristian Costa <cristiancosta1991@gmail.com>
2025-06-23 11:44:04 -0300

    Adding sing-up swagger specification

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -18,6 +18,25 @@ const router = express.Router();
  *        refreshToken:
  *          type: string
  *          description: Refresh token
+ *    SignUpResponse:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: number
+ *          description: User identifier
+ *          example: 1
+ *        username:
+ *          type: string
+ *          description: User name
+ *          example: myusername
+ *        createdAt:
+ *          type: string
+ *          description: User created at
+ *          example: 2025-06-22T20:13:10.325Z
+ *        updatedAt:
+ *          type: string
+ *          description: User updated at
+ *          example: 2025-06-22T20:13:10.325Z
  *    InvalidCredentialsResponse:
  *      type: object
  *      properties:
@@ -39,14 +58,21 @@ const router = express.Router();
  *          type: string
  *          description: Error message
  *          example: INTERNAL_SERVER_ERROR
+ *    UserAlreadyExistResponse:
+ *      type: object
+ *      properties:
+ *        message:
+ *          type: string
+ *          description: Error message
+ *          example: USER_ALREADY_EXIST
  */
 
 /**
  * @swagger
  * /auth/sign-in:
  *  post:
- *    summary: Authenticate user
- *    description: Authenticate user
+ *    summary: Sign in user
+ *    description: Sign in user
  *    tags:
  *      - Authentication
  *    requestBody:
@@ -94,6 +120,51 @@ const router = express.Router();
  */
 router.post('/sign-in', authController.signIn);
 
+/**
+ * @swagger
+ * /auth/sign-up:
+ *  post:
+ *    summary: Sign up user
+ *    description: Sign up user
+ *    tags:
+ *      - Authentication
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              username:
+ *                type: string
+ *                description: User name
+ *                example: myusername
+ *              password:
+ *                type: string
+ *                description: User password
+ *                example: pa55w0rd
+ *            required:
+ *              - username
+ *              - password
+ *    responses:
+ *      200:
+ *        description: Created user
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/SignUpResponse'
+ *      409:
+ *        description: Conflict
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/UserAlreadyExistResponse'
+ *      500:
+ *        description: Internal server error
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/InternalServerErrorResponse'
+ */
 router.post('/sign-up', authController.signUp);
 
 router.post('/refresh', authController.refresh);
