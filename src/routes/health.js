102259ab5e99c10a270ae5c0f4a35d641b70a1aa
Cristian Costa <cristiancosta1991@gmail.com>
2025-06-25 12:54:23 -0300

    Adding health swagger specification

--- src/routes/health.js
+++ src/routes/health.js
@@ -6,9 +6,45 @@ const healthRoute = (database) => {
   // Controllers.
   const healthController = require('../controllers/health')(database);
 
+  /**
+   * @swagger
+   * /health:
+   *  get:
+   *    summary: Health information
+   *    description: API and database health information
+   *    tags:
+   *      - Health
+   *    responses:
+   *      200:
+   *        description: API and database health information
+   *        content:
+   *          application/json:
+   *            schema:
+   *              $ref: '#/components/schemas/HealthResponse'
+   */
   router.get('/', healthController.getHealthInfo);
 
   return router;
 };
 
 module.exports = healthRoute;
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    HealthResponse:
+ *      type: object
+ *      properties:
+ *        status:
+ *          type: string
+ *          description: API status
+ *          example: healthy
+ *        dependencies:
+ *          type: object
+ *          properties:
+ *            database:
+ *              type: string
+ *              description: Database status
+ *              example: connected
+ */
